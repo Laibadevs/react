b630cb991b29307c3eb38ee864df744334902db7
Laibadevs <laibalrb68@gmail.com>
2026-04-22 10:27:34 +0500

    react done

---
+++ react/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+function App()
+{
+  
+  return
+  <>
+  <div className="dad">
+    <h1 id="child">Hello</h1>
+    <h2 id="child">Hello</h2>
+  </div>
+  <div id="Chacha"></div>
+  </>
+
+}
+export default App
