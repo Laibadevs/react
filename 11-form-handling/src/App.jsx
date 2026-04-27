a9f47a134720b7e3e13d08eb663752c39ff37f6f
Laibadevs <laibalrb68@gmail.com>
2026-04-27 11:20:47 +0500

    form

---
+++ 11-form-handling/src/App.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+const App=()=>
+{
+  //problem page reload everytime when submit so overcome this problem use preventdefault
+  const submitHandler=(e)=>
+  {
+  e.preventDefault()
+  console.log("form submit")
+  }
+  return(
+    <div>
+      <form onSubmit={(e)=>{
+        submitHandler(e)
+      }}>
+        <input type="text" placeholder="Enter your name"/>
+        
+        <button>Submit</button>
+      </form>
+    </div>
+  )
+}
+export default App
