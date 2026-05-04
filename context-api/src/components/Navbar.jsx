ca33d5ff85e549e0d53a36156190ab7ac40426cf
Laibadevs <laibalrb68@gmail.com>
2026-05-04 11:52:11 +0500

    done

---
+++ context-api/src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import { useContext } from 'react'
+import Navbar2 from './Navbar2'
+import { ThemeDataContext } from '../context/ThemeContext'
+
+const Navbar = () => {
+
+    const [theme] = useContext(ThemeDataContext)
+    return (
+        <div className={theme}>
+            <h2>Sheryians</h2>
+            <Navbar2 />
+        </div>
+    )
+}
+
+export default Navbar
